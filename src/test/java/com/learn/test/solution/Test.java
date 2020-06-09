afb205c36e14777ec97f45adfa717dda8c4ec111
n490808114 <Zzz8801668>
2020-06-09 10:04:03 +0800

    add test file

---
+++ src/test/java/com/learn/test/solution/Test.java
@@ -0,0 +1,8 @@
+package com.learn.test.solution;
+
+public class Test {
+    public static void main(String[] args) {
+        Integer i = 20;
+        System.out.println(String.valueOf(i));
+    }
+}
